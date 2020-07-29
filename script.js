bcc6eacdfa87ee84d89bd9b46bd5188fe8a667b0
Anita <dananiuta@gmail.com>
2020-07-29 17:12:31 -0400

    saving for push

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function check() {
+function check(event) {
+   // event.preventDefault()
     var question1 = document.quiz.question1.value
     var question2 = document.quiz.question2.value
     var question3 = document.quiz.question3.value
@@ -8,23 +9,32 @@ function check() {
 
     if (question1 == "Kyiv") {
         correct++;
+        
     }
+    
     if (question2 == "Kiwi") {
         correct++;
+        
     }
 
     if (question3 == "Mediterranian Sea") {
         correct++;
+        
     }
 
     if (question4 == "none of the above") {
         correct++;
+        
     }
 
     if (question5 == "Valentina Tereshkova") {
         correct++;
+        
     }
     var alerts = ["Well done!", "That wasn't bad. Cheer up! I'm sorry, you failed!"];
+alert(alerts[0])
+
+    console.log(correct)
 
     var range;
     if (correct < 1) {
@@ -39,14 +49,38 @@ function check() {
     if (correct > 2) {
         range = 0;
     }
-    var countDownTime = new Time("10:00").getTime();
-
-    
-
+    //var countDownTime = new Time("10:00").getTime();
 
 
 
     document.getElementById("after_submit").style.visability = "visible";
     document.getElementById("correct_answers").innerHTML = "You got " + correct + "correct."
+clearInterval(interval)
+
+        $(".timer").text(countDownTime + " - You got " + correct + " correct.")
 
 }
+var countDownTime = 20;
+
+
+var interval;
+
+interval = setInterval(function () {
+    if (countDownTime > 0) {
+        countDownTime--
+        $(".timer").text(countDownTime)
+
+    } else {
+        clearInterval(interval)
+
+        $(".timer").text(countDownTime + " - Game Over!")
+
+    }
+
+}, 1000)
+
+
+
+
+
+
